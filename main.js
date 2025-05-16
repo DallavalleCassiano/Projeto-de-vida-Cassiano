8419c0dedd77113ef2a4a1c1a8b720efee581f31
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2025-05-16 10:57:31 -0300

    dale

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const botoes = document.querySelectorAll(",botao");
 
 for (let i=0; i<botoes.length;i++){
     botoes[I].onclick = function (){
-        
+
+        for(let j=0;j<botoes.length; j++){
+            botoes[j].classList.revome("ativo");
+        }
+
+        botoes[i].classList.add ("ativo");
     }
 }
