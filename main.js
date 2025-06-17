f3dee7603d3956807e245a07ded60a0ea391cd4d
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2025-06-17 07:57:01 -0300

    salvo

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ for (let i = 0; i < botoes.length; i++) {
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo")
-       }
+        }
 
         botoes[i].classList.add("ativo");
-     botoes[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");
 
     }
 }
@@ -19,4 +19,4 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-01-10T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0]. textContent = tempoObjetivo1- tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
