e2a74ca6dd81f4da4595eccb9096e3e7fe94d724
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2025-07-01 08:20:04 -0300

    top

--- main.js
+++ main.js
@@ -34,8 +34,14 @@ function calculaTempo(tempoObjetivo){
     minutos %=60;
     horas %=60;
 
-    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
-}
+    if(tempoFinal>0){
+        return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+    }else{
+          return"Prazo Finalizado"
+    }
+     }
+
+    
 
 function atualizaCronometro(){
     for (let i=0; i<contadores.length; i++){
