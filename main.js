53252ae829936a1719e819e42015d0c715d74e85
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2025-07-08 08:44:57 -0300

    s

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-01-05T00:00:00");
-const tempoObjetivo2 = new Date("2026-02-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-01-01T00:00:00");
+const tempoObjetivo2 = new Date("2026-02-01T00:00:00");
 const tempoObjetivo3 = new Date("2026-08-31T00:00:00");
 const tempoObjetivo4 = new Date("2026-12-01T00:00:00");
 
@@ -29,26 +29,22 @@ function calculaTempo(tempoObjetivo){
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
-}
 
     segundos %= 60;
     minutos %=60;
     horas %=24;
 
-    if(tempoFinal>0){
-        return [dias, horas, minutos, segundos];
-    }else{
-          return[0,0,0,0]
-     }
-
     
+    if(tempoFinal>0){
+        return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+    }else{
+        return "Prazo Finalizado!"
+    }
+}
 
 function atualizaCronometro(){
     for (let i=0; i<contadores.length; i++){
-        document.getElementById("dias" = + i). textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas" = + i). textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min" = + i). textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg" = + i). textContent = calculaTempo(tempos[i])[3];
+        contadores[i].textContent=calculaTempo(tempos[i]);
     }
 }
 
